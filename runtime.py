8d8aac881f67a59d0eca367249bcd1fadc01e6e9
jone-the-creator <turaptor@gmail.com>
2026-04-20 20:37:00 +0930

    Clarity updates

--- runtime.py
+++ runtime.py
@@ -1,9 +1,13 @@
-from Classes import Quadcopter, AdapterManager, UsbTransport, ProtocolAdapter
-from Comms_Plugins.CRTP import CRTP
+from Classes import Quadcopter
+from Comms_Plugins.Crazyradio import CRTP_logger
 
-if __name__ == "__main__":
+# the following runtime will only be run when script is run, NOT when imported
+if __name__ == "__main__": 
     quad = Quadcopter("quad_001")
-    comms = CRTP(quad)
+    
+    #comms plugin will be selected here based on selection in GUI
+    if 1:
+        comms = CRTP_logger(quad)
 
     print("Runtime started. Press Ctrl+C to stop.")
 
