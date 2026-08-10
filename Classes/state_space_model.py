9f2020f420e824fe6d854fb3897313512b6a04c6
jone-the-creator <turaptor@gmail.com>
2026-08-10 11:15:54 +0930

    state space model prepared

--- Classes/state_space_model.py
+++ Classes/state_space_model.py
@@ -1,41 +1,71 @@
 import numpy as np
 import state_space as ss
-from quadcopter import Quadcopter
 
 g = 9.81 # m/s^2
 
 class Model:
-    def __init__(self, quad):
+    def __init__(self, quadcopter):
+        self.quad = quadcopter
         self.x = np.array([
-            [quad.position.x],
-            [quad.position.y],
-            [quad.position.z],
-            [quad.velocity.x],
-            [quad.velocity.y],
-            [quad.velocity.z],
-            [quad.attitude.roll],
-            [quad.attitude.pitch],
-            [quad.attitude.yaw],
-            [quad.velocity.roll],
-            [quad.velocity.pitch],
-            [quad.velocity.yaw]
+            [self.quad.position.x],
+            [self.quad.position.y],
+            [self.quad.position.z],
+            [self.quad.velocity.x],
+            [self.quad.velocity.y],
+            [self.quad.velocity.z],
+            [self.quad.attitude.roll],
+            [self.quad.attitude.pitch],
+            [self.quad.attitude.yaw],
+            [self.quad.velocity.roll],
+            [self.quad.velocity.pitch],
+            [self.quad.velocity.yaw]
         ])
 
         self.A = np.array([
             [0, 0, 0, 1, 0, 0, 0,  0, 0, 0, 0, 0],
             [0, 0, 0, 0, 1, 0, 0,  0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 1, 0,  0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, -g, 0, 0, 0, 0]
+            [0, 0, 0, 0, 0, 0, 0, -g, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, g,  0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0,  0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0]
         ])
 
         self.B = np.array([
-            []
+            [0,                0,                0,                0               ],
+            [0,                0,                0,                0               ],
+            [0,                0,                0,                0               ],
+            [0,                0,                0,                0               ],
+            [1/self.quad.mass, 0,                0,                0               ],
+            [0,                0,                0,                0               ],
+            [0,                0,                0,                0               ],
+            [0,                0,                0,                0               ],
+            [0,                0,                0,                0               ],
+            [0,                1/self.quad.I_xx, 0,                0               ],
+            [0,                0,                1/self.quad.I_yy, 0               ],
+            [0,                0,                0,                1/self.quad.I_zz],
+
         ])
 
         self.C = np.array([
-            []
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
         ])
 
         self.D = np.array([
-            []
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
         ])
